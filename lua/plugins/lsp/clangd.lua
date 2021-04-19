e72ff6fa45ea16997a0a8cf1372dd7413d2ed450
zit <tobias.zindl@leica-geosystems.com>
2021-04-19 07:12:43 +0200

    added clangd as lsp

---
+++ lua/plugins/lsp/clangd.lua
@@ -0,0 +1,19 @@
+
+local clangd_root_path =  vim.fn.stdpath('data') .. "/lspinstall/cpp/clangd"
+local clangd_script    = clangd_root_path .. "/bin/clangd"
+
+
+
+require'lspconfig'.clangd.setup {
+    cmd = {clangd_script},
+   -- on_attach = require'lsp'.common_on_attach,
+   -- handlers = {
+   --     ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+   --         virtual_text = true,
+   --         signs = true,
+   --         underline = true,
+   --         update_in_insert = true
+
+   --     })
+   -- }
+}
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -3,6 +3,7 @@ require('plugins.lsp.completion')
 
 -- language server
 require('plugins.lsp.lua-ls')
+require('plugins.lsp.clangd')
 
 -- formatter
 require('plugins.lsp.efm')

1c6e131e39bff66eaa6efa2a357209c63be0c412
Tobias Zindl <zie.develop@gmail.com>
2022-04-10 18:55:55 +0200

    remove tabnine

--- lua/zie/lsp/init.lua
+++ lua/zie/lsp/init.lua
@@ -3,7 +3,6 @@ local source_mapping = {
   buffer = "[Buffer]",
   nvim_lsp = "[LSP]",
   nvim_lua = "[Lua]",
-  cmp_tabnine = "[TN]",
   path = "[Path]",
 }
 
@@ -73,34 +72,18 @@ cmp.setup {
     format = function(entry, vim_item)
       vim_item.kind = lspkind.presets.default[vim_item.kind]
       local menu = source_mapping[entry.source.name]
-      if entry.source.name == "cmp_tabnine" then
-        if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-          menu = entry.completion_item.data.detail .. " " .. menu
-        end
-        vim_item.kind = ""
-      end
       vim_item.menu = menu
       return vim_item
     end,
   },
 
   sources = {
-    { name = "cmp_tabnine" },
     { name = "nvim_lsp" },
     -- For luasnip user.
     { name = "luasnip" },
     { name = "buffer" },
   },
   completion = { completeopt = "menu,menuone,noinsert" },
-}
-
-local tabnine = require "cmp_tabnine.config"
-tabnine:setup {
-  max_lines = 1000,
-  max_num_results = 20,
-  sort = true,
-  run_on_every_keystroke = true,
-  snippet_placeholder = "..",
 }
 
 -- Use an on_attach function to only map the following keys

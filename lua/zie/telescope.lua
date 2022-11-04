85c18a1fb0ae6b0fded6988916d928d299c6fa2e
Tobias Zindl <zie.develop@gmail.com>
2022-11-04 09:43:42 +0100

    [telescope]: add repo module

--- lua/zie/telescope.lua
+++ lua/zie/telescope.lua
@@ -9,6 +9,11 @@ require("telescope").setup {
       case_mode = "smart_case", -- or "ignore_case" or "respect_case"
       -- the default case_mode is "smart_case"
     },
+    repo = {
+      list = {
+        search_dirs = { "~/Workspace/work", "~/Workspace/zie87" },
+      },
+    },
   },
 }
 -- To get fzf loaded and working with telescope, you need to call
@@ -16,6 +21,9 @@ require("telescope").setup {
 require("telescope").load_extension "fzf"
 require("telescope").load_extension "file_browser"
 require("telescope").load_extension "dap"
+
+vim.g['rooter_cd_cmd'] = 'lcd'
+require("telescope").load_extension "repo"
 require("telescope").load_extension "session-lens"
 -- require("telescope").load_extension "cmake"
 
